598562623a384397e901ff85509692a3738d9fd8
Sherub Thakur <sherub.thakur@gmail.com>
2023-09-16 01:29:58 +0530

    Add git initialize

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,26 @@
 module Main (main) where
 
+import Options.Applicative (command, execParser, fullDesc, header, helper, progDesc, subparser, (<**>))
+import Options.Applicative.Builder (info)
+import Options.Applicative.Types (Parser)
+
 import Lib
 
+data Command = Init
+
 main :: IO ()
-main = someFunc
+main = runCommand =<< execParser opts
+  where
+    opts =
+        info
+            (commands <**> helper)
+            ( fullDesc
+                <> progDesc "A distributed VCS written in haskell"
+                <> header "hagit - a haskell git clone"
+            )
+
+commands :: Parser Command
+commands = subparser (command "init" (info (pure Init) (progDesc "Initialize a git repository")))
+
+runCommand :: Command -> IO ()
+runCommand Init = initialize
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,19 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (
+    initialize,
+) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import System.Directory (createDirectoryIfMissing)
+import System.FilePath ((</>))
+import System.IO (IOMode (WriteMode), hPutStrLn, withFile)
+
+gitLocation :: FilePath
+gitLocation = "git_test"
+
+initialize :: IO ()
+initialize = do
+    let createParents = True
+    createDirectoryIfMissing createParents (gitLocation </> "objects")
+    createDirectoryIfMissing createParents (gitLocation </> "refs")
+    withFile (gitLocation </> "HEAD") WriteMode $ \f -> do
+        hPutStrLn f "ref: refs/heads/master"
+    putStrLn "Initialized git directory"
